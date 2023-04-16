a2e4e19aee68512323cd7423a8cadb1b286310ce
aarver <bacsajozseff@gmail.com>
2023-04-16 17:19:24 +0200

    bacsajozsef

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { HomeComponent } from './home/home.component';
+import { Nav1Component } from './nav1/nav1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: MainComponent},
+  { path: 'home', component: HomeComponent, children: [
+    { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
+    { path: 'portfolio', component: PortfolioComponent },
+  ]},
+  { path: 'home', component: HomeComponent, children: [
+    { path: '', redirectTo: 'aboutme', pathMatch: 'full' },
+    { path: 'aboutme', component: AboutmeComponent },
+  ]},
+  { path: 'home', component: HomeComponent, children: [
+    { path: '', redirectTo: 'resume', pathMatch: 'full' },
+    { path: 'resume', component: ResumeComponent },
+  ]},
+  { path: 'home', component: HomeComponent, children: [
+    { path: '', redirectTo: 'testimonial', pathMatch: 'full' },
+    { path: 'testimonial', component: TestimonialsComponent },
+  ]},
+  { path: 'home', component: HomeComponent, children: [
+    { path: '', redirectTo: 'aboutme', pathMatch: 'full' },
+    { path: 'contact', component: ContactComponent },
+  ]},
+  {path: '**', redirectTo: 'main', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { Nav1Component } from './nav1/nav1.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutmeComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    NavbarComponent,
+    HomeComponent,
+    Nav1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
