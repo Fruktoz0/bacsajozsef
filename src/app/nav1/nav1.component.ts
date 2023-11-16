ca1907a812cc0e346c53fad2dccccd5d500409e5
aarver <bacsajozseff@gmail.com>
2023-11-16 13:26:17 +0100

    frissítés

--- src/app/nav1/nav1.component.ts
+++ src/app/nav1/nav1.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-nav1',
   templateUrl: './nav1.component.html',
   styleUrls: ['./nav1.component.scss']
 })
-export class Nav1Component {
+export class Nav1Component implements OnInit {
+
+
+  menuOpen = false;
+
+  ngOnInit() {
+
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+
+  }
 
 }
